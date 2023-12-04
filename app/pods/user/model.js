978f96e6e63f6c924cbe22607d0f930072968e05
DawidDamek <dawid.damek98@gmail.com>
2023-12-04 20:42:10 +0100

    add description to user model

--- app/pods/user/model.js
+++ app/pods/user/model.js
@@ -4,6 +4,7 @@ export default class UserModel extends Model {
   @attr('string') name;
   @attr('string') surname;
   @attr('string') email;
+  @attr('string') descriiption;
   @attr('string') phoneNumber;
   @attr('string') degree;
   @attr('string') photoUrl;
