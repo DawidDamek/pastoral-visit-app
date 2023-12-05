0f9254ee0e6de63848e43039e7ef68c3e27cfbf7
DawidDamek <dawid.damek98@gmail.com>
2023-12-05 20:29:56 +0100

    add login form, bootstrap and api adapter

---
+++ app/pods/components/login-form/component.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class LoginFormComponent extends Component {
+  @tracked credentials = { password: null, email: null };
+
+  @action
+  onPropertyChange(key, { target: { value } }) {
+    this.credentials[key] = value;
+    console.log(this.credentials[key]);
+  }
+  @action
+  onLogin() {
+    console.log('login');
+  }
+}
